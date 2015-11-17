148d05edd86a329bdc5a873e524cea705ddd299a
JT <joshua.seger@hudl.com>
2015-11-16 19:53:38 -0600

    Making good progress. Branching point.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+	attr_accessor 
+	has_secure_password
+	validates :first_name,  presence: true, length: { maximum: 50 }, allow_blank: false
+	validates :last_name,  presence: true, length: { maximum: 50 }, allow_blank: false
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }, allow_blank: false
+    validates :team_id, presence: false, allow_blank: false
+    before_save   :downcase_email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+  get 'signup69' => 'signup#new'
+  get 'home' => 'home#index'
+  get 'welcome', to:'welcome#about', as:'about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
