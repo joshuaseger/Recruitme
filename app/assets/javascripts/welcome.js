80bec06cf3bd4b67d92286d9dcfce08e25b22add
JT <joshua.seger@hudl.com>
2015-12-02 12:02:55 -0600

    Making good progress on css

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function(){
+	console.log("DOM Loaded");
+
+	//Close popup on x-icon clicked.
+	$('#x-icon').click(function() {
+		$('.container-popup').css('display', 'none');
+	});
+
+	//Show sign up popup
+	$('#nav-sign-up').click(function(){
+		$('.container-popup').css('display', 'block');
+	});
+});
+
